0843f0ad616728ac98c1347121f0ab28de786641
Bogdan <sevr1k@bigmir.net>
2017-05-16 21:00:45 +0300

    Update task3

--- ViewModels/Task3ViewModel.cs
+++ ViewModels/Task3ViewModel.cs
@@ -35,54 +35,33 @@ namespace WpfTask.ViewModels
 
         public async void SomeM(object obj)
         {
-            await kek();
-            //for (int i = 0; i < 10000; i++)
-            //{
-            //    SomeCollection.Add("asd "+ i);
-            //    //Thread myThread = new Thread(Count);
-            //    //myThread.Name = "Поток " + i.ToString();
-            //    //myThread.Start();
-            //}
-        }
-
-        public async Task  kek()
-        {
-            await Task.Run(() =>
+            for (int i = 0; i < 9; i++)
             {
-                for (var i = 0; i < 10000; i++)
-                {
-                    Thread.Sleep(100);
-                    App.Current.Dispatcher.Invoke(
-                        delegate
-                        {
-                            SomeCollection.Add("asd " + i.ToString());
-                        });
-                }
-                
-            });
-            //for (int i = 0; i < 10000; i++)
-            //{
-            //     SomeCollection.Add("asd " + i);
-            //    //Thread myThread = new Thread(Count);
-            //    //myThread.Name = "Поток " + i.ToString();
-            //    //myThread.Start();
-            //}
-
+                Thread myThread = new Thread(Count);
+                myThread.Name = "Поток " + i.ToString();
+                myThread.Start();
+            }
         }
-        
 
         public async void Count()
         {
-            mutexObj.WaitOne();
+            await Task.Run((Action) delegate
+                {
+                    mutexObj.WaitOne();
+                }
+            );
+
             x = 1;
             for (int i = 1; i < 9; i++)
             {
-                SomeCollection.Add($"{Thread.CurrentThread.Name}: {x}");
-                //Console.WriteLine("{0}: {1}", Thread.CurrentThread.Name, x);
+                App.Current.Dispatcher.Invoke((Action) delegate
+                {
+                    SomeCollection.Add($"X variable is - {x}");
+                });
                 x++;
-                Thread.Sleep(100);
+                Thread.Sleep(10);
             }
-            mutexObj.ReleaseMutex();
+             mutexObj.ReleaseMutex();
         }
     }
 }
